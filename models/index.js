631af1a4b834ffa2bfb228b24adfc2e380c040d4
Tyler Harper <tyler@tylerharper.dev>
2022-11-27 11:41:23 -0800

    convert project to use postgres db

--- models/index.js
+++ models/index.js
@@ -1,16 +1,18 @@
 "use strict";
 
+require("dotenv").config();
 const fs = require("fs");
 const path = require("path");
 const Sequelize = require("sequelize");
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || "development";
 const config = require(__dirname + "/../config/config.json")[env];
+
 const db = {};
 
 let sequelize;
 if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+  sequelize = new Sequelize(process.env.DB_URL, config);
 } else {
   sequelize = new Sequelize(
     config.database,
